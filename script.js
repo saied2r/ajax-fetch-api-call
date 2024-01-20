a5f0e0a84a702e5b22d7c0c6b2af1dccec2d4140
saied2r <saiedtechit@gmail.com>
2024-01-20 23:29:24 +0600

    read data form txt file

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-// In this file we will read data form "data.json"
+// loadDataFromJSON function load data form a .json file
+// loadDataFromTXT function load data form a .txt  file
 
-const xmlHttp = new XMLHttpRequest();
-xmlHttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200){
-        const data  = this.responseText;
-        console.log(JSON.parse(data))
+const loadDataFromJSON = () =>{ 
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200){
+            const data  = this.responseText;
+            console.log(JSON.parse(data))
+        }
     }
+    
+    xmlHttp.open("GET", "/data.json", true)
+    xmlHttp.send()
 }
 
-xmlHttp.open("GET", "/data.json", true)
-xmlHttp.send()
+
+const loadDataFromTXT = () =>{ 
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/text.txt", true);
+    xhr.onload = function(){
+        if (this.status === 200){
+            console.log(this.responseText)
+        }
+        else{
+            alert("text not found");
+        }
+
+    }
+    xhr.send();
+    }
+
+
+loadDataFromJSON();
+loadDataFromTXT();
+
+
+
