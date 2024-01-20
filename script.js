93017c2894c5d2515184a821fb6a133b06015a38
saied2r <saiedtechit@gmail.com>
2024-01-20 11:51:22 +0600

    read JSON form another file

---
+++ script.js
@@ -0,0 +1,12 @@
+// In this file we will read data form "data.json"
+
+const xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200){
+        const data  = this.responseText;
+        console.log(data);
+    }
+}
+
+xmlHttp.open("GET", "/data.json", true)
+xmlHttp.send()
