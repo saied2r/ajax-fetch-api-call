774c33e124a0d3c23ef3e6417081ffb9f23bca2e
saied2r <saiedtechit@gmail.com>
2024-01-20 10:52:06 +0600

    add post method

--- fetch.js
+++ fetch.js
@@ -1,4 +1,11 @@
-fetch("https://jsonplaceholder.typicode.com/users")
+//const object_that_will_post =
+// it will get from user and designed as object
+
+fetch("https://jsonplaceholder.typicode.com/users", {
+    method: 'POST',
+    body: JSON.stringify(object_that_will_post),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+})
 .then(res => res.json())
 .then(data => console.log(data))
 .catch(err => console.log(err))
